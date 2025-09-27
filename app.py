5bc4e8b71d177f57623b445bc1d357f76c3b9240
Nery008 <gabrielleneryslima@gmail.com>
2025-09-27 10:52:53 -0300

    Desenvolvimento_web_Flask

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from datetime import datetime
+import json
+import resend
+
+resend.api_key = 're_TE1Muh5P_4ywxRLmrPpCUskQWGQm94JBy' # chave da API para envio de mensagem
+
+
+app = Flask(__name__)  # instanciamos a aplicação web
+
+with open('dados.json', 'r', encoding='utf-8') as arquivo:
+    dados = json.load(arquivo)
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        nome=request.form['name']
+        email=request.form['email']
+        mensagem=request.form['message']
+
+        dados_mensagem = {
+            'nome': nome,
+            'email':email,
+            'mensagem':mensagem,
+            'data' :f'{datetime.today()}'
+        }
+        dados.append(dados_mensagem)
+        with open('dados.json','w',encoding='utf-8') as arquivo:
+            json.dump(dados, arquivo, indent=4, ensure_ascii=False)
+            
+       
+            r= resend.Emails.send({
+                    "from":"onboarding@resend.dev",
+                    "to":"gabrielleneryslima@gmail.com",
+                    "subject": f"Solicitação de adoção {nome}",
+                     "html": f'<p>Email:{email}<br>{mensagem}</p>'
+                })
+        return redirect(url_for('index'))
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
